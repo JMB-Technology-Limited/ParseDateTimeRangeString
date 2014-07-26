6b88efd2510e90bc4dfb7ffc97e1a065ef140af4
James <james@jarofgreen.co.uk>
2014-07-26 08:50:08 +0100

    Rename tests so they actually run, doh!

--- tests/JMBTechnologyLimited/ParseDateTimeRangeString/ParseDateTimeRangeStringStartEndEnGbTest.php
+++ tests/JMBTechnologyLimited/ParseDateTimeRangeString/ParseDateTimeRangeStringStartEndEnGbTest.php
@@ -9,7 +9,7 @@ namespace JMBTechnologyLimited\ParseDateTimeRangeString;
  * @copyright (c) 2013-2014, JMB Technology Limited, http://jmbtechnology.co.uk/
  * @author James Baster <james@jarofgreen.co.uk>
  */
-class ParseDateTimeRangeStringStartEndTestEnGb extends \PHPUnit_Framework_TestCase{
+class ParseDateTimeRangeStringStartEndEnGbTest extends \PHPUnit_Framework_TestCase{
 
 	
 	function startEndProvider() {
