1855342102482261fea5d61f7857eb94fbbce47f
James <james@jarofgreen.co.uk>
2015-11-26 09:52:37 +0000

    STRICT mode in tests closes https://github.com/JMB-Technology-Limited/ParseDateTimeRangeString/issues/30

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 
+error_reporting( E_STRICT );
+
 require __DIR__.'/../src/JMBTechnologyLimited/ParseDateTimeRangeString/ParseDateTimeRangeString.php';
 require __DIR__.'/../src/JMBTechnologyLimited/ParseDateTimeRangeString/ParseDateTimeRangeStringResult.php';
-
